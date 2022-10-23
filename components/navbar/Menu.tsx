bef0d224d99ea7bd193872c458dd8accea6e6745
Maximiliano <maxylujan1@outlook.com>
2022-10-23 12:24:09 -0300

    Adding the desktop layout

--- components/navbar/Menu.tsx
+++ components/navbar/Menu.tsx
@@ -1,5 +1,6 @@
-import { ArrowRightAlt } from "@mui/icons-material";
-import { Box, List , ListItem,IconButton,ListItemText, Button} from "@mui/material";
+import * as React from 'react';
+import { Box} from "@mui/material";
+import ListItems from './ListItems';
 
 export default function Menu(props:any){
     return(<Box component={"nav"} sx={{
@@ -13,62 +14,6 @@ export default function Menu(props:any){
         backgroundColor: '#FFFFFF',
         padding: '.2rem'
     }} className={props.className}>
-        <List sx={{ margin:'auto', width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            <ListItem  disableGutters secondaryAction={
-                <IconButton>
-                    <ArrowRightAlt/>
-                </IconButton>
-            }>
-                <ListItemText primary={`Laptops`} />
-            </ListItem>
-            <ListItem  disableGutters secondaryAction={
-                <IconButton>
-                    <ArrowRightAlt/>
-                </IconButton>
-            }>
-                <ListItemText primary={`Desktop PCs`} />
-            </ListItem>
-            <ListItem  disableGutters secondaryAction={
-                <IconButton>
-                    <ArrowRightAlt/>
-                </IconButton>
-            }>
-                <ListItemText primary={`Networking Devices`} />
-            </ListItem>
-            <ListItem  disableGutters secondaryAction={
-                <IconButton>
-                    <ArrowRightAlt/>
-                </IconButton>
-            }>
-                <ListItemText primary={`Printers & Scanners`} />
-            </ListItem>
-            <ListItem  disableGutters secondaryAction={
-                <IconButton>
-                    <ArrowRightAlt/>
-                </IconButton>
-            }>
-                <ListItemText primary={`Pc Parts`} />
-            </ListItem>
-            <ListItem  disableGutters secondaryAction={
-                <IconButton>
-                    <ArrowRightAlt/>
-                </IconButton>
-            }>
-                <ListItemText primary={`All other Products`} />
-            </ListItem>
-            <ListItem  disableGutters secondaryAction={
-                <IconButton>
-                    <ArrowRightAlt/>
-                </IconButton>
-            }>
-                <ListItemText primary={`Repairs`} />
-            </ListItem>
-        </List>
-        <Button variant={'outlined'} sx={{
-            width:'50%',
-            borderRadius:'25px',
-            border: 'solid 2px #0156FF',
-            margin:'auto'
-        }}>Our Deals</Button>
+        <ListItems/>
     </Box>)
 }
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import { useState } from 'react';
 
-import { Box ,TextField,InputAdornment } from "@mui/material";
+import { Box ,TextField,InputAdornment, useMediaQuery } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import SearchIcon from '@mui/icons-material/Search';
 import InfoMenu from "./InfoMenu"
+import ShopSharp  from '@mui/icons-material/ShopSharp';
+import Menu from './Menu';
+import ListItems from './ListItems';
+import Link from 'next/link';
 
 export default function Navbar(){
+    const isSmallerTablet = useMediaQuery("(min-width:810px)")
+    const isBiggerDesktop = useMediaQuery("(min-width:1000px)")
     const [visible,setVisible] = useState(false);
     const [seeMenu,setSeeMenu] = useState(false)
     const handleClickSearch = () =>{
@@ -26,46 +32,103 @@ export default function Navbar(){
         }
     }
     return(
-        <Box sx={{
-            backgroundColor: '#FFFFFF',
-            display: {
-                tablet: 'none',
-            }
-        }}>
-            <Box sx={{
-                    height: '3rem',
-                    margin: '0',
-                    display: 'flex',
-                    alignItems: 'center',
-                    backgroundColor: '#0156FF'
+        <>
+            <Box
+                display={
+                    isSmallerTablet ? 'none':'flex'
+                }
+                sx={{
+                    backgroundColor: '#FFFFFF',
                 }}>
-                    <MenuIcon onClick={handleClickMenu} htmlColor='#ffffff' sx={{
-                        flex:'1',
-                        cursor: 'pointer'
-                    }}/>
-                    <TextField  sx={{flex:'5',backgroundColor:'#FFFFFF',borderRadius:'25px',padding:'.1rem'}}
-                        id="input-with-icon-textfield"
-                        InputProps={{
-                            disableUnderline:true,
-                        startAdornment: (
-                        <InputAdornment position="start">
-                            <SearchIcon />
-                        </InputAdornment>
-          ),
-        }}
-        variant="standard"
-      />
-                    <LocalGroceryStoreIcon htmlColor='#FFFFFF' sx={{
-                        flex:'1'
-                    }}/>
-                    <AccountCircleIcon htmlColor='#FFFFFF' sx={{
-                        flex:'1'
-                    }}/>
-                    
+                <Box sx={{
+                        height: '3rem',
+                        margin: '0',
+                        display: 'flex',
+                        alignItems: 'center',
+                        backgroundColor: '#0156FF',
+                        width: '100%'
+                    }}>
+                        <MenuIcon onClick={handleClickMenu} htmlColor='#ffffff' sx={{
+                            flex:'1',
+                            cursor: 'pointer'
+                        }}/>
+                        <TextField  sx={{flex:'5',backgroundColor:'#FFFFFF',borderRadius:'25px',padding:'.1rem'}}
+                            id="input-with-icon-textfield"
+                            InputProps={{
+                                disableUnderline:true,
+                            startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon />
+                            </InputAdornment>
+            ),
+            }}
+            variant="standard"
+        />
+                        <LocalGroceryStoreIcon htmlColor='#FFFFFF' sx={{
+                            flex:'1'
+                        }}/>
+                        <AccountCircleIcon htmlColor='#FFFFFF' sx={{
+                            flex:'1'
+                        }}/>
+                        
+                </Box>
+                <>
+                    <InfoMenu menu={seeMenu}/>
+                </>
             </Box>
-            <>
-                <InfoMenu menu={seeMenu}/>
-            </>
-        </Box>
+            <Box display={
+                isSmallerTablet?'flex':'none'
+            } sx={{
+                alignItems:'center',
+                justifyContent:'space-between',
+                width:'100%',
+                height:'3rem'
+            }}>
+                <LocalGroceryStoreIcon sx={{flex: '1'}} htmlColor='#000000'/>
+                <Box sx={{
+                    display:'flex',
+                    alignItems:'center',
+                    justifyContent:'space-between',
+                    flex:'8'
+                }}>
+                    {(visible) 
+                    ?<TextField sx={{alignItems:'center',}}></TextField>
+                    :<Box sx={{display:'flex',alignItems:'center'}}>
+                        <nav style={{margin:'auto'}}>
+                            <ul style={{display:'flex',listStyle:'none'}}>
+                                <li>
+                                    <Link href={'/'} ><a style={{fontSize:'12px',margin:'0 .3rem'}}>Laptops</a></Link>
+                                </li>
+                                <li>
+                                    <Link href={'/'}><a style={{fontSize:'12px',margin:'0 .3rem'}}>Desktop PCs</a></Link>
+                                </li>
+                                <li>
+                                    <Link href={'/'}><a style={{fontSize:'12px',margin:'0 .3rem'}}>Networking Devices</a></Link>
+                                </li>
+                                <li>
+                                    <Link href={'/'}><a style={{fontSize:'12px',margin:'0 .3rem'}}>Printers & Scanners</a></Link>
+                                </li>
+                                <li>
+                                    <Link href={'/'}><a style={{fontSize:'12px',margin:'0 .3rem'}}>PC Parts</a></Link>
+                                </li>
+                                <li>
+                                    <Link href={'/'}><a style={{fontSize:'12px',margin:'0 .3rem'}}>All Other Products</a></Link>
+                                </li>
+                                <li>
+                                    <Link href={'/'} ><a style={{fontSize:'12px',margin:'0 .3rem'}}>Repairs</a></Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </Box>}
+                    <SearchIcon  htmlColor='#000000' onClick={handleClickSearch}/>
+                </Box>
+                <Box sx={{
+                    display: 'flex'
+                }}>
+                    <ShopSharp sx={{flex: '1'}} htmlColor='#000000'/>
+                    <AccountCircleIcon sx={{flex: '1'}} htmlColor='#000000'/>
+                </Box>
+            </Box>
+        </>
     )
 }

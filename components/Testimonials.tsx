f0f09f41ca1851a6b0ee6cfe63a716fb52c09102
Maximiliano <maxylujan1@outlook.com>
2022-10-26 10:42:25 -0300

    Adding form and modal for an users review

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,39 +1,87 @@
-import { Box, Radio, RadioGroup, Typography } from '@mui/material';
+import { Box, Radio, RadioGroup, Typography , Button , Modal, TextField, TextareaAutosize  } from '@mui/material';
 import * as React from 'react';
 import { useState } from 'react';
 import {testimonials} from "../src/constants/testimonials"
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import SendIcon from '@mui/icons-material/Send';
 
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 
 export default function Testimonials(){
-    const [review,setReview] = useState(0)
+    const [review,setReview] = useState(0);
+    const [open, setOpen] = React.useState(false);
     let aux;
 
+
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     const handleChange = (event: any) => {
         setReview(event.target.value);
       };
-    
+
     if (testimonials[review]){
         aux = {id:testimonials[review].id,author:testimonials[review].author,review:testimonials[review].review};
     } else{
         aux = {error:true}
     }
-    
+    const handleSubmit = () =>{
+        alert("subir nueva review")
+    }
+
     return( <Box>
                 <Box>
-                    {aux.error
-                    ?<Typography>The testimony is not available</Typography>
-                    :<Typography>{aux.review}</Typography>}
-                </Box>
-                <Box>
-                    {aux.error
-                    ?<Typography>The author is not available</Typography>
-                    :<Typography>{aux.author}</Typography>}
-                </Box>
-                <Box>
-
                     <Box>
+                        <FormatQuoteIcon/>
+                    </Box>
+                    <Box>
+                        {aux.error
+                        ?<Typography component={'h2'}>The testimony is not available</Typography>
+                        :<Typography component={'blockquote'}>{aux.review}</Typography>}
+                    </Box>
+                </Box>
+                <Box>
+                    {aux.error
+                    ?<Typography component={'h2'}>The author is not available</Typography>
+                    :<Typography component={'h3'}>{aux.author}</Typography>}
+                </Box>
+                <Box>
+                    <Box>
+                        <Button onClick={handleOpen} variant="outlined">Leave us a review</Button>
                         <RadioGroup>
+                        <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box sx={style}>
+                                <Typography id="modal-modal-title" variant="h6" component="h2">
+                                    Leave us a review
+                                </Typography>
+                                <form onSubmit={handleSubmit}>
+                                    <TextField></TextField>
+                                    <TextareaAutosize
+                                        aria-label="empty textarea"
+                                        placeholder="Empty"
+                                        style={{ width: 200 }}
+                                        />
+                                    <Button variant={'outlined'} endIcon={<SendIcon/>}>Send Review</Button>
+                                </form>
+                            </Box>
+                        </Modal>
                             <Radio onClick={handleChange} value={0}/>
                             <Radio onClick={handleChange} value={1}/>
                             <Radio onClick={handleChange} value={2}/>
--- src/constants/testimonials.js
+++ src/constants/testimonials.js
@@ -7,20 +7,20 @@ export const testimonials = [
     },
     {
         id: 2,
-        author: 'Tama Brown',
-        review:'lalalalalalalalalalalalalalal'
+        author: 'Tomas Diaz',
+        review:'I found much more than I expected, their dedication to the customer surpasses any other previous experience, they always respond to my doubts with total courtesy and solve any problem with efficiency. For someone like me who is just starting in the Internet market I must say it has been a wise decision.'
         
     },
     {
         id: 3,
-        author: 'Tama Brown',
-        review:'My first order arrived today in perfect condition.  From the time I sent a question about the item to making the purchase, to the shipping and now the delivery, your company, Tecs, has stayed in touch.  Such great service.  I look forward to shopping on your site in the future and would highly recommend it.'
+        author: 'Erik Smith',
+        review:'I have always taken into account good service, punctuality and responsibility, but what you offered us was more than that, it was a completely personalized service, very organized and above all very creative, at all times coordinated with us and gave us advice for our website.'
         
     },
     {
         id: 4,
-        author: 'Tama Brown',
-        review:'My first order arrived today in perfect condition.  From the time I sent a question about the item to making the purchase, to the shipping and now the delivery, your company, Tecs, has stayed in touch.  Such great service.  I look forward to shopping on your site in the future and would highly recommend it.'
+        author: 'Gonzalo Torres',
+        review:'From the very first moment, they helped me with all the smallest details regarding hosting, domains and web pages. They were always available and helped me with the same enthusiasm as in the beginning. For that I can only thank them. I recommend their services 100%.'
         
     }
 ]

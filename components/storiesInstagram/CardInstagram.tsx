71e7ab4692c7e268ecf5ad063aca73ebf85d0c16
Maximiliano <maxylujan1@outlook.com>
2022-11-08 13:26:16 -0300

    Doing card instagram news

--- components/storiesInstagram/CardInstagram.tsx
+++ components/storiesInstagram/CardInstagram.tsx
@@ -5,12 +5,10 @@ import { Grid } from '@mui/material';
 
 export default function cardInstagram(props:any){
     return(
-        <Grid item>
-            <Box maxWidth={'235px'}  sx={{margin:'.5rem',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+        <Grid item xs={12} sm={6} md={8} sx={{maxWidth:'235px',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
                 <img style={{width:'235px',height:'152px'}} src={props.card.image}></img>
                 <Typography component={'p'} sx={{fontSize:'12px',fontWeight:'400',padding:'.2rem'}}>{props.card.new}</Typography>
                 <Typography  component={'p'} sx={{color:'#A2A6B0',fontSize:'10px',fontWeight:'400'}}>{props.card.date}</Typography>
-            </Box>
         </Grid>
     )
 }
--- components/storiesInstagram/CardsInstagram.tsx
+++ components/storiesInstagram/CardsInstagram.tsx
@@ -1,14 +1,14 @@
 import { Grid } from '@mui/material';
 import * as React from 'react';
 import newsInstagram from '../../src/constants/cardInstagram';
-import CardInstagram from './cardInstagram';
+import CardInstagram from './CardInstagram';
 
 
 
 export default function CardsInstagram(){
 
     return(
-        <Grid maxWidth={'1398px'} container spacing={1} rowGap={'gap'} justifyContent='center' alignItems='center' margin='0 auto'>
+        <Grid sx={{width:'100%'}} maxWidth={'1398px'} container spacing={2} rowGap={'gap'} justifyContent='center' alignItems='center' margin='0 auto'>
             {newsInstagram.map((el,ind) => {
                 if (ind > 11) return false;
                 return <CardInstagram key={el.id} card={el}/>
